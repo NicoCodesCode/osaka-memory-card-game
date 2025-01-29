0df2ba76c7842be2d11b05bc161f8aca0a537bf6
Nicolás Ramírez <nicolasrmz08@gmail.com>
2025-01-29 07:34:13 -0600

    change style

--- src/components/TItleScreen.jsx
+++ src/components/TItleScreen.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function TitleScreen() {
   return (
-    <main className="w-screen h-screen bg-gradient-to-b from-orange-400 to-orange-600 flex justify-center items-center">
+    <main className="min-w-screen min-h-screen bg-gradient-to-b from-orange-400 to-orange-600 flex justify-center items-center">
       <article className="maz-w-2/3 mx-auto p-8 flex flex-col gap-8 items-center">
         <img
           src="/assets/azumanga-logo.png"
@@ -12,15 +12,17 @@ export default function TitleScreen() {
         <h1 className="text-4xl sm:text-5xl font-bold text-center text-daioh-yellow py-2">
           Osaka Memory Card Game
         </h1>
-        <Link to="/play">
-          <button className="bg-yellow-200 text-orange-700 font-bold py-2 px-6 rounded-lg shadow-md hover:scale-110 transition ease-in-out">
-            Play
-          </button>
-        </Link>
+        <div className="z-10">
+          <Link to="/play">
+            <button className="bg-yellow-200 text-orange-700 font-bold py-2 px-6 rounded-lg shadow-md hover:scale-110 transition ease-in-out">
+              Play
+            </button>
+          </Link>
+        </div>
         <img
           src="/assets/title-osaka.png"
           alt="osaka menu image"
-          className="absolute bottom-0 opacity-80 md:max-w-80"
+          className="absolute bottom-0 opacity-75 md:max-w-80"
         />
       </article>
     </main>
